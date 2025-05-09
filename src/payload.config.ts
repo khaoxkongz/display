3dc0d717db5e11866d02eda8eab31d54da49e9ab
Kim Hanz <nanaka3@hotmail.com>
2025-05-10 00:45:38 +0700

    Add TourTravels collection and update payload configuration

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -10,6 +10,7 @@ import sharp from "sharp"
 import { Media } from "./collections/Media"
 import { TourStudies } from "./collections/TourStudies"
 import { Users } from "./collections/Users"
+import { TourTravels } from "./collections/TourTravels"
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -21,7 +22,7 @@ export default buildConfig({
       baseDir: path.resolve(dirname),
     },
   },
-  collections: [Users, Media, TourStudies],
+  collections: [Users, Media, TourStudies, TourTravels],
   editor: lexicalEditor(),
   secret: process.env.PAYLOAD_SECRET || "",
   typescript: {
